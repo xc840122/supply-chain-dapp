ebcba1d71eb863c91c680a4c31ad05642457ecb8
Chi Xu <xuchi007@hotmail.com>
2025-05-28 17:56:32 +1200

    config context api

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { TrackingProvider } from '@/Context/Tracking';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <TrackingProvider {...pageProps}>
+        < Component {...pageProps} />
+      </TrackingProvider>  // Pass pageProps to TrackingProvider
+    </>
+  )
 }
